344b9773196efd52868ecaeaf4a00d30baec1ff4
2-Eung <eung4256@gmail.com>
2025-06-18 10:50:28 +0900

    List .indexOf() .set() .subList(,)

--- src/Main.java
+++ src/Main.java
@@ -13,20 +13,20 @@ public class Main {
         fruits.add("Apple");
         fruits.add("Mango");
 
-        fruits.remove("DragonFruit");   // 직접 지우기
-        fruits.remove(3);            // 인덱스로 지우기
+        int index = fruits.indexOf("Banana");   // () 안이 포함되어있으면 해당 인덱스 출력
+        System.out.println(index);
+        int index2 = fruits.indexOf("Kiwi");    // 포함되어있지않으면 -1 을 출력
+        System.out.println(index2);
 
-        for (String fruit : fruits) {
-            System.out.println(fruit);
+        fruits.set(1,"Apple");                  // 해당 인덱스 내용을 수정한다
+        System.out.println(fruits.get(1));      // get() () 안의 인덱스 를 읽어온다
+        int index3 = fruits.indexOf("Apple");   // 포함한 첫번째 인덱스
+        System.out.println(index3);
+
+        List<String> subFruits = fruits.subList(0, 2);  // .subList 기존리스트 를 잘라서 새로만든다
+        // 입력값1 인덱스 부터 입력값2 인덱스 전 까지
+        for(String subFruit: subFruits) {
+            System.out.println(subFruit);
         }
-
-        System.out.println(fruits.size());
-        System.out.println(fruits.isEmpty());
-
-        fruits.clear();
-
-        System.out.println(fruits.size());
-        System.out.println(fruits.isEmpty());       // 장바구니 비었는지 체크
-
     }
 }
